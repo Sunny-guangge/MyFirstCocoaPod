8b4acdaa8c71397349d46dce5ab2e55bbfc2239c
sunny_Wang <a@123>
2015-10-12 17:09:45 +0800

    加入工程

---
+++ MyFirstCocoaPod/MyFirstCocoaPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstCocoaPod
+//
+//  Created by user on 15/10/12.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
